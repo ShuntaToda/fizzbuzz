f400ab44a11110269c21a7df41480ac50c04f70c
ShuntaToda <todashuntawork@gmail.com>
2023-12-07 18:41:22 +0900

    関数化

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,25 @@
+# for i in range(1,101):
+#   if i % 5 == 0 and i % 3 == 0:
+#     print("FizzBuzz")
+#   elif i % 3 == 0:
+#     print("Fizz")
+#   elif i % 5 == 0:
+#     print("Buzz")
+#   else:
+#     print(i)
+
+
+def fizzbuzz(num: int) -> int:
+    if num % 5 == 0 and num % 3 == 0:
+      return "FizzBuzz"
+    elif num % 3 == 0:
+      return "Fizz"
+    elif num % 5 == 0:
+      return "Buzz"
+    else:
+      return num
+    
+
+
 for i in range(1,101):
-  if i % 5 == 0 and i % 3 == 0:
-    print("FizzBuzz")
-  elif i % 3 == 0:
-    print("Fizz")
-  elif i % 5 == 0:
-    print("Buzz")
-  else:
-    print(i)
+  print(fizzbuzz(i))
