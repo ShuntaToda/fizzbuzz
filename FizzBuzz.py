401437fd3c968b461815443c83b06acebe0dad79
ShuntaToda <todashuntawork@gmail.com>
2023-12-12 16:39:58 +0900

    リスト内包表記を使ったUI分離

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,15 +1,5 @@
-# for i in range(1,101):
-#   if i % 5 == 0 and i % 3 == 0:
-#     print("FizzBuzz")
-#   elif i % 3 == 0:
-#     print("Fizz")
-#   elif i % 5 == 0:
-#     print("Buzz")
-#   else:
-#     print(i)
 
-
-def fizzbuzz(num: int) -> int:
+def fizzbuzz(num: int) -> str:
     if num % 5 == 0 and num % 3 == 0:
       return "FizzBuzz"
     elif num % 3 == 0:
@@ -17,9 +7,9 @@ def fizzbuzz(num: int) -> int:
     elif num % 5 == 0:
       return "Buzz"
     else:
-      return num
+      return str(num)
     
 
+results = [fizzbuzz(i) for i in range(1,101)]
 
-for i in range(1,101):
-  print(fizzbuzz(i))
+print("\n".join(results))
